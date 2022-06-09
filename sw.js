c4990f117a8ecefc872cdb67235014b2b06d6d33
sjtio <samuel@sjt.io>
2022-06-10 00:51:24 +0100

    offline support + icons

--- sw.js
+++ sw.js
@@ -1,7 +1,58 @@
 var GHPATH = '/simple-pwa-test';
 var APP_PREFIX = 'pwat_';
-var VERSION = 'version_00';
+var VERSION = 'version_01';
 var URLS = [
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
 ]
+var CACHE_NAME = APP_PREFIX + VERSION
+self.addEventListener('fetch', function (e) {
+  console.log('Fetch request : ' + e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        console.log('Responding with cache : ' + e.request.url);
+        return request
+      } else {
+        console.log('File is not cached, fetching : ' + e.request.url);
+        return fetch(e.request)
+      }
+    })
+  )
+})
+
+// self.addEventListener("fetch", fetchEvent => {
+//   fetchEvent.respondWith(
+//     caches.match(fetchEvent.request).then(res => {
+//       return res || fetch(fetchEvent.request)
+//     })
+//   )
+// })
+
+
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      cache.addAll(URLS)
+    })
+  )
+})
+
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      cacheWhitelist.push(CACHE_NAME);
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('Deleting cache : ' + keyList[i] );
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
+})
